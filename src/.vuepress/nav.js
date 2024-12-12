c6f3c3a29085a46c89e4cbab9c89511e626a7bfa
周必川 <zhoubichuan@icloud.com>
2024-12-12 18:56:39 +0800

    更新文档

--- src/.vuepress/nav.js
+++ src/.vuepress/nav.js
@@ -1,26 +1,28 @@
 const nav = [
     {
         text: '基础知识',
-        items: [{
-            text: '一.基础学习',
-            link: '/base/build/1.index'
-        },
-        {
-            text: '二.实战练习',
-            link: '/base/practice/1.index',
-        }
+        items: [
+            {
+                text: '一.基础学习',
+                link: '/base/build/1.index'
+            },
+            {
+                text: '二.实战练习',
+                link: '/base/practice/1.index',
+            }
         ]
     },
     {
         text: '高级知识',
-        items: [{
-            text: '一.项目实战',
-            link: '/senior/use/1.index'
-        },
-        {
-            text: '二.其他',
-            link: '/senior/typeScript/1.index'
-        },
+        items: [
+            {
+                text: '一.项目实战',
+                link: '/senior/use/1.index'
+            },
+            {
+                text: '二.其他',
+                link: '/senior/typeScript/1.index'
+            },
         ]
     }
 ];
