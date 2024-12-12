2900375b0040bb2beb988f826b42a40750223959
周必川 <zhoubichuan@icloud.com>
2024-12-12 17:27:47 +0800

    修改配置

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,130 +1,106 @@
 const path = require("path");
+
 module.exports = {
-  title: 'MongoDB学习笔记',
-  description: '风浪没平息 我宣告奔跑的意义',
-  base: '/web-mongodb/', // 部署站点的基础路径
+  title: "MongoDB学习笔记",
+  description: "风浪没平息 我宣告奔跑的意义",
+  base: "/web-mongodb/", // 部署站点的基础路径
   port: 3009,
-  dest: 'web-mongodb', // 指定 vuepress 的输出目录
-  markdown: {
-    toc: { includeLevel: [2, 3] },
-    lineNumbers: true // 代码块显示行号
-  },
   alias: {
     "common-node": path.resolve(__dirname, "../../common-node"),
   },
-  // head: [
-  //   ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css' }],
-  //   ['link', { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css" }]
-  // ],
-
-  plugins: {
-    // 'vuepress-plugin-mermaidjs': {},
-    // "vuepress-plugin-auto-sidebar": {},
-    'demo-container': true,
-    // 名称：@vuepress/plugin-pwa 网页内容有更新的时候有刷新按钮。可以把网页保存到桌面，当一个app一样
-    '@vuepress/pwa': {
-      serviceWorker: true,
-      updatePopup: true
-    },
-    // 名称：@vuepress/plugin-back-to-top 效果：文章看到下面的时候，点击一个图标会回到顶部
-    '@vuepress/back-to-top': true,
-    '@vuepress/medium-zoom': true,
-    '@vuepress/nprogress': true,
-    // 名称：@vuepress/plugin-active-header-links 效果：页面滚动时自动激活侧边栏链接的插件，效果就是右边内容滚动的时候，看到哪里了，左侧菜单会自动高亮显示当前看的目录。
-    '@vuepress/active-header-links': {
-      sidebarLinkSelector: '.sidebar-link',
-      headerAnchorSelector: '.header-anchor'
-    },
-    "@vssue/vuepress-plugin-vssue": {
-      platform: 'github', //v3的platform是github，v4的是github-v4
-      locale: 'zh', //语言
-      // 其他的 Vssue 配置
-      owner: 'zhoubichuan', //github账户名
-      repo: 'web-mongodb', //github一个项目的名称
-      clientId: 'Iv1.2923ba5d4de48a3c', //注册的Client ID
-      clientSecret: '110210', //注册的Client Secret
-      autoCreateIssue: true // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了。
-    },
-    "vuepress-plugin-boxx": ["vuepress-plugin-boxx"]
+  dest: "web-mongodb", // 指定 vuepress 的输出目录
+  markdown: {
+    toc: { includeLevel: [2, 3] },
+    lineNumbers: true, // 代码块显示行号
   },
+  plugins: [
+    [
+      "vuepress-plugin-anchor-toc",
+      {
+        showDepth: 1,
+        customClass: "your-customClass",
+        ignore: ["/", "/api/"],
+      },
+    ],
+    ['fulltext-search'],
+    // 只要把这个放进 config的plugins中就可以了
+    [
+      "sakura",
+      {
+        num: 20, // 默认数量
+        show: true, //  是否显示
+        zIndex: -1, // 层级
+        img: {
+          replace: false, // false 默认图 true 换图 需要填写httpUrl地址
+          httpUrl: "...", // 绝对路径
+        },
+      },
+    ],
+    [
+      // 名称：@vuepress/plugin-pwa 网页内容有更新的时候有刷新按钮。可以把网页保存到桌面，当一个app一样
+      "@vuepress/pwa",
+      {
+        serviceWorker: true,
+        updatePopup: {
+          message: "有新的内容更新",
+          buttonText: "刷新",
+        },
+      },
+    ],
+    [
+      "copyright",
+      {
+        authorName: "zhoubichuan@icloud.com", // 选中的文字将无法被复制
+        minLength: 30, // 如果长度超过  30 个字符
+      },
+    ],
+    // [
+    //   "vuepress-plugin-nuggets-style-copy",
+    //   {
+    //     copyText: "复制代码",
+    //     tip: {
+    //       content: "复制成功",
+    //     },
+    //   },
+    // ],
+    ["@vuepress/back-to-top"],
+    ["@vuepress/medium-zoom"],
+    ["@vuepress/nprogress"],
+    [
+      // 名称：@vuepress/plugin-active-header-links 效果：页面滚动时自动激活侧边栏链接的插件，效果就是右边内容滚动的时候，看到哪里了，左侧菜单会自动高亮显示当前看的目录。
+      "@vuepress/active-header-links",
+      {
+        sidebarLinkSelector: ".sidebar-link",
+        headerAnchorSelector: ".header-anchor",
+      },
+    ],
+    [
+      "@vssue/vuepress-plugin-vssue",
+      {
+        platform: "github", //v3的platform是github，v4的是github-v4
+        locale: "zh", //语言
+        // 其他的 Vssue 配置
+        owner: "zhoubichuan", //github账户名
+        repo: "web-mongodb", //github一个项目的名称
+        clientId: "Iv1.2923ba5d4de48a3c", //注册的Client ID
+        clientSecret: "110210", //注册的Client Secret
+        autoCreateIssue: true, // 自动创建评论，默认是false，最好开启，这样首次进入页面的时候就不用去点击创建评论的按钮了
+      },
+    ],
+    ["vuepress-plugin-boxx"],
+  ],
   // 主题配置
   themeConfig: {
-    repo: 'https://github.com/zhoubichuan/web-mongodb', // 源码地址
-    repoLabel: '查看源码', // (查看源码的)组件名称
-    docsBranch: 'master', // git 源仓库 仓库分支
+    repo: "https://gitee.com/zhoubichuan/web-mongodb", // 源码地址
+    repoLabel: "查看源码", // (查看源码的)组件名称
+    docsBranch: "master", // git 源仓库 仓库分支
     editLinks: true, // 编辑链接
-    docsDir: 'src', // 假如文档不是放在仓库的根目录下
-    editLinkText: '在github上编辑此页',
+    docsDir: "src", // 假如文档不是放在仓库的根目录下
+    editLinkText: "在gitee上编辑此页",
     sidebarDepth: 2,
-    lastUpdated: '上次更新', // 获取每个文件最后一次 git 提交的时间戳
-    nav: [{
-      text: '基础知识',
-      items: [{
-        text: '一.基础学习',
-        link: '/base/build/1.index'
-      },
-      {
-        text: '二.实战练习',
-        link: '/base/practice/1.index',
-      }
-      ]
-    },
-    {
-      text: '高级知识',
-      items: [{
-        text: '一.项目实战',
-        link: '/senior/use/1.index'
-      },
-      {
-        text: '二.其他',
-        link: '/senior/typeScript/1.index'
-      },
-      ]
-    }
-    ],
-    sidebar: {
-      '/base/build/': [
-        '1.index',
-        '2.webpack',
-        '3.file',
-        '4.single',
-        '5.page',
-        '7.module',
-        '8.project',
-        '9.utils',
-        '10.ui',
-        '11.data',
-        '12.skill',
-        '13.com',
-        '14.data',
-        '15.api'
-      ],
-      '/base/practice/': [
-        '1.index',
-        '2.Jenkins',
-        '3.gitlab',
-        'node',
-        'express',
-        'koa',
-        'egg'
-      ],
-      '/senior/use/': [
-        '1.index',
-        '2.extend',
-        '3.componentCommunication',
-        '4.render',
-        '5.api',
-        '6.form',
-        '7.checkBox',
-        '8.active',
-        '9.alert',
-        '10.table',
-        '11.tree'
-      ],
-      '/senior/typeScript/': [
-        '1.index'
-      ],
-    },
-    searchMaxSuggestoins: 10
+    lastUpdated: "上次更新", // 获取每个文件最后一次 git 提交的时间戳
+    nav: require("./nav.js"),
+    sidebar: require("./sidebar.js"),
+    searchMaxSuggestoins: 10,
   },
-}
+};
---
+++ src/.vuepress/sidebar.js
@@ -0,0 +1,44 @@
+const sidebar = {
+    '/base/build/': [
+        '1.index',
+        '2.webpack',
+        '3.file',
+        '4.single',
+        '5.page',
+        '7.module',
+        '8.project',
+        '9.utils',
+        '10.ui',
+        '11.data',
+        '12.skill',
+        '13.com',
+        '14.data',
+        '15.api'
+    ],
+    '/base/practice/': [
+        '1.index',
+        '2.Jenkins',
+        '3.gitlab',
+        'node',
+        'express',
+        'koa',
+        'egg'
+    ],
+    '/senior/use/': [
+        '1.index',
+        '2.extend',
+        '3.componentCommunication',
+        '4.render',
+        '5.api',
+        '6.form',
+        '7.checkBox',
+        '8.active',
+        '9.alert',
+        '10.table',
+        '11.tree'
+    ],
+    '/senior/typeScript/': [
+        '1.index'
+    ],
+};
+module.exports = sidebar;
